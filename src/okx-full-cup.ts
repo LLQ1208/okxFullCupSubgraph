09a74ab754aba9e9e49e6ed55cca77decce2882c
linqiang.liang <linqiang.liang@okg.com>
2022-11-11 16:26:22 +0800

    update

--- src/okx-full-cup.ts
+++ src/okx-full-cup.ts
@@ -1,115 +1,42 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import {TransferBatch, TransferSingle} from "../generated/okxFullCup/okxFullCup";
+
 import {
-  okxFullCup,
-  AdminChanged,
-  ApprovalForAll,
-  BeaconUpgraded,
-  Canceled,
-  ClaimEnabled,
-  Initialized,
-  OwnershipTransferred,
-  TransferBatch,
-  TransferSingle,
-  URI,
-  UnStaked,
-  Upgraded,
-  WithdrawEnabled
-} from "../generated/okxFullCup/okxFullCup"
-import { ExampleEntity } from "../generated/schema"
+  TransferSingle as TransferSingleEntity,
+  TransferBatch as TransferBatchEntity
+} from "../generated/schema"
 
-export function handleAdminChanged(event: AdminChanged): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleTransferSingle(event: TransferSingle): void {
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract._pauseClaimMap(...)
-  // - contract.balanceOf(...)
-  // - contract.balanceOfAll(...)
-  // - contract.balanceOfBatch(...)
-  // - contract.bonusContract(...)
-  // - contract.claimEnabled(...)
-  // - contract.claimPauseTimeMap(...)
-  // - contract.exists(...)
-  // - contract.getBlockList(...)
-  // - contract.getImplementation(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.mintEndTime(...)
-  // - contract.mintPauseTimeMap(...)
-  // - contract.mintStartTime(...)
-  // - contract.mintedAmountOf(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.proxiableUUID(...)
-  // - contract.snapshot(...)
-  // - contract.stakeAmountMap(...)
-  // - contract.stakeContract(...)
-  // - contract.stakePrice(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.totalHolder(...)
-  // - contract.totalHolder(...)
-  // - contract.totalMintForDrop(...)
-  // - contract.totalStaking(...)
-  // - contract.totalSupply(...)
-  // - contract.totalSupply(...)
-  // - contract.uri(...)
-  // - contract.withdrawEnabled(...)
+  let entity = new TransferSingleEntity(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
+  entity.operator = event.params.operator.toHex()
+  entity.from = event.params.from.toHex()
+  entity.to = event.params.to.toHex()
+  entity.param_id = event.params.id
+  entity.value = event.params.value
+  entity.save();
 }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
 
-export function handleBeaconUpgraded(event: BeaconUpgraded): void {}
+export function handleTransferBatch(event: TransferBatch): void {
 
-export function handleCanceled(event: Canceled): void {}
+  let entity = new TransferBatchEntity(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
 
-export function handleClaimEnabled(event: ClaimEnabled): void {}
-
-export function handleInitialized(event: Initialized): void {}
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
-
-export function handleTransferBatch(event: TransferBatch): void {}
-
-export function handleTransferSingle(event: TransferSingle): void {}
-
-export function handleURI(event: URI): void {}
-
-export function handleUnStaked(event: UnStaked): void {}
-
-export function handleUpgraded(event: Upgraded): void {}
-
-export function handleWithdrawEnabled(event: WithdrawEnabled): void {}
+  entity.operator = event.params.operator.toHex()
+  entity.from = event.params.from.toHex()
+  entity.to = event.params.to.toHex()
+  let ids = event.params.ids;
+  let idsStr = "[";
+  for(let i=0;i<ids.length;i++){
+    idsStr = idsStr + "," + ids[i].toHex()
+  }
+  idsStr = idsStr.substring(0,idsStr.length-1) + "]";
+  entity.ids = idsStr
+  let values = event.params.values;
+  let valuesStr = "[";
+  for(let i=0;i<values.length;i++){
+    valuesStr = valuesStr + "," + values[i].toHex()
+  }
+  valuesStr = valuesStr.substring(0,idsStr.length-1) + "]";
+  entity.values = valuesStr
+  entity.save();
+}
